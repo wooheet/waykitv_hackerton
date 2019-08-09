16fa4f5ff65f99507ad76c8317ac5c57d668bd33
tedy <tedy@r2v.io>
2019-08-09 23:48:55 +0900

    Add tx types

--- routes/users.js
+++ routes/users.js
@@ -27,17 +27,58 @@ router.get('/', function(req, res, next) {
   //   const contractLocation =  path.join(__dirname, "../contract/contract-hello.lua");
   // let script = fs.readFileSync(contractLocation);
 
+    /*
+    WiccApi.PROTOCAL_VERSION = 1;
+      WiccApi.REGISTER_ACCOUNT_TX = 2;
+      WiccApi.COMMON_TX = 3;
+      WiccApi.CONTRACT_TX = 4;
+      WiccApi.REG_APP_TX = 5,   	//!< register app
+      WiccApi.DELEGATE_TX = 6;
+     */
+
+    //REGISTER_ACCOUNT_TX 2
   let txInfo = {
       nTxType: 2,         //REGISTER_ACCOUNT_TX
       nVersion: 1,
       nValidHeight: 219831,
-      fees: 10,
+      fees: 10000000,
       pubkey: privateKey.toPublicKey().toString(),
       minerPubkey: ''
   };
 
-    // console.log(api.REGISTER_ACCOUNT_TX)
-  let rawTx =  api.createSignTransaction(privateKey, 2, txInfo)
+    //COMMON_TX 3
+
+    var commonTxinfo = {
+        nTxType: 3,
+        nVersion: 1,
+        nValidHeight: 602371,
+        fees: 10000,
+        srcRegId: '54528-1',
+        destAddr: 'wZPQNFru7fWVyWqmcFEY6zZ51HudiKA29C',
+        value:1,
+        network: 'testnet'
+    };
+
+    //CONTRACT_TX 4
+    // var regAppInfo = {
+    //     nTxType: 4,
+    //     nVersion: 1,
+    //     nValidHeight: 34400,    // create height
+    //     srcRegId: "22030-2",    // sender's regId
+    //     destRegId: "24555-1",  // app regId
+    //     fees: 1000000,         // fees pay for miner
+    //     value: 1,              // amount of WICC to be sent to the app account
+    //     vContract: "f018"      // contract method, hex format string
+    // };
+
+    // let commonTx = wicc.Transaction.CommonTx(commonTxinfo)
+    // let hex = commonTx.SerializeTx(privateKey)
+    // let ret = commonTx._SignatureHash()
+    // console.log(ret.toString('hex'))
+    // let result = commonTx._Signtx(privateKey);
+    // console.log(result)
+
+    let rawTx =  api.createSignTransaction(privateKey, 3, commonTxinfo)
     console.log("reg app tx raw: ")
     console.log(rawTx)
 
